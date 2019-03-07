073ad20cabd139e363f8a98b5ed1fc9770f0691a
Rafael Barros <rafaelbarros.df@gmail.com>
2019-03-07 20:33:09 -0300

    :construction: Serialização com JAXB

---
+++ 14-serializacao-com-jaxb/loja/src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -0,0 +1,23 @@
+package br.com.alura.loja.modelo;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Produto {
+
+	private long id;
+	private String nome;
+	private double preco;
+	private int quantidade;
+	
+}
