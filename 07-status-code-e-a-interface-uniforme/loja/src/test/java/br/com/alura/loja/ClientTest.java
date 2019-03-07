e1ad1f56bb5adb5af3310bb14f8fac79eb08f021
Rafael Barros <rafaelbarros.df@gmail.com>
2019-03-07 15:23:04 -0300

    :construction: Status code e a Interface Uniforme

---
+++ 07-status-code-e-a-interface-uniforme/loja/src/test/java/br/com/alura/loja/ClientTest.java
@@ -0,0 +1,65 @@
+package br.com.alura.loja;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
+
+public class ClientTest {
+	private HttpServer server;
+	private Client client;
+
+	@Before
+	public void startaServidor() {
+		server = Servidor.inicializaServidor();
+		client = ClientBuilder.newClient();
+	} 
+	
+	@After
+	public void mataServidor() {
+		server.stop();
+	}
+
+	@Test
+	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/carrinhos/1").request().get(String.class);
+
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		
+		assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+	}
+	
+	@Test
+	public void testaQueSuportaNovosCarrinhos(){
+		WebTarget target = client.target("http://localhost:8080");
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+		carrinho.setRua("Rua Vergueiro");
+		carrinho.setCidade("Sao Paulo");
+		String xml = carrinho.toXML();
+		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+		
+		Response response = target.path("/carrinhos").request().post(entity);
+		assertEquals(201, response.getStatus());
+		
+		String location = response.getHeaderString("Location");
+		String conteudo = client.target(location).request().get(String.class);
+		assertTrue(conteudo.contains("Tablet"));
+	}
+}
