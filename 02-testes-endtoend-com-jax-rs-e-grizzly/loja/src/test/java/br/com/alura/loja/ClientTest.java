e89c79852abdc433b7a3d00f620cd65006bd0991
Rafael Barros <rafaelbarros.df@gmail.com>
2019-03-07 14:03:23 -0300

    :construction: Testes endtoend com JAXRS e Grizzly

---
+++ 02-testes-endtoend-com-jax-rs-e-grizzly/loja/src/test/java/br/com/alura/loja/ClientTest.java
@@ -0,0 +1,41 @@
+package br.com.alura.loja;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+
+public class ClientTest {
+	private HttpServer server;
+
+	@Before
+	public void startaServidor() {
+		server = Servidor.inicializaServidor();
+	} 
+	
+	@After
+	public void mataServidor() {
+		server.stop();
+	}
+
+	@Test
+	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/carrinhos").request().get(String.class);
+
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		
+		assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+	}
+}
